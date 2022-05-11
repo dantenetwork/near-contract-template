b004015f316de6f6e4509f78a5e7f5e4b827f1e1
Kay <kay20475@hotmail.com>
2022-05-11 17:16:47 +0800

    * register and send message

--- dante-cross-chain-standards/src/core_impl.rs
+++ dante-cross-chain-standards/src/core_impl.rs
@@ -1,50 +1,94 @@
-use crate::types::{Content, DstContract};
+use crate::types::{Content, DstContract, Session};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
-use near_sdk::serde_json::json;
-use near_sdk::{ext_contract, AccountId, IntoStorageKey, Promise, Gas};
+use near_sdk::{env, ext_contract, AccountId, Balance, Gas, IntoStorageKey, PromiseOrValue};
 
 const GAS_FOR_SENT_MESSAGE: Gas = Gas(5_000_000_000_000);
 
+const NO_DEPOSIT: Balance = 0;
 
 #[ext_contract(ext_cross_contract)]
 pub trait FungibleTokenContract {
-    fn send_message(&mut self, to_chain: String, content: Content);
+    fn send_message(&mut self, to_chain: String, content: Content, session: Option<Session>) -> u64;
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Standards {
+    pub owner_id: AccountId,
     pub cross_chain_contract_id: AccountId,
     pub destination_contract: UnorderedMap<String, DstContract>,
 }
 
 impl Standards {
-    pub fn new<S>(prefix: S, cross_chain_contract_id: AccountId) -> Self
+    pub fn new<S>(prefix: S, owner_id: AccountId, cross_chain_contract_id: AccountId) -> Self
     where
         S: IntoStorageKey,
     {
         let this = Self {
+            owner_id,
             cross_chain_contract_id,
             destination_contract: UnorderedMap::new(prefix),
         };
         this
     }
-    
-    pub fn send_message(
-        &self,
-        to_chain: String,
-        content: Content
-    ) {
-        Promise::new(self.cross_chain_contract_id.clone()).function_call(
-            "send_message".to_string(),
-            json!({
-                "to_chain": to_chain,
-                "content": content,
-            })
-            .to_string()
-            .into_bytes(),
-            0,
+
+    pub fn call_cross(&self, to_chain: String, content: Content) {
+        ext_cross_contract::send_message(
+            to_chain,
+            content,
+            None,
+            self.cross_chain_contract_id.clone(),
+            NO_DEPOSIT,
             GAS_FOR_SENT_MESSAGE,
         );
     }
+
+    pub fn register_dst_contract(
+        &mut self,
+        chain_name: String,
+        contract_address: String,
+        action_name: String,
+    ) {
+        assert_eq!(env::predecessor_account_id(), self.owner_id, "Unauthorize");
+        self.destination_contract.insert(
+            &chain_name,
+            &DstContract {
+                contract_address,
+                action_name,
+            },
+        );
+    }
+
+    pub fn call_cross_with_session(
+        &self,
+        to_chain: String,
+        content: Content,
+    ) -> PromiseOrValue<u64> {
+        ext_cross_contract::send_message(
+            to_chain,
+            content,
+            Some(Session{res_type: 1, id: None}),
+            self.cross_chain_contract_id.clone(),
+            NO_DEPOSIT,
+            GAS_FOR_SENT_MESSAGE,
+        )
+        .into()
+    }
+
+    pub fn send_response_message(
+        &self,
+        to_chain: String,
+        content: Content,
+        id: u64,
+    ) {
+        ext_cross_contract::send_message(
+            to_chain,
+            content,
+            Some(Session{res_type: 2, id: Some(id)}),
+            self.cross_chain_contract_id.clone(),
+            NO_DEPOSIT,
+            GAS_FOR_SENT_MESSAGE,
+        );
+    }
+
 }
--- dante-cross-chain-standards/src/types.rs
+++ dante-cross-chain-standards/src/types.rs
@@ -10,6 +10,13 @@ pub struct Content {
 }
 
 #[derive(Clone, PartialEq, BorshDeserialize, BorshSerialize, Serialize, Deserialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Session {
+    pub res_type: u8,
+    pub id: Option<u64>,
+}
+
+#[derive(Clone, PartialEq, BorshDeserialize, BorshSerialize, Serialize, Deserialize, Debug)]
 #[serde(tag = "type", crate = "near_sdk::serde")]
 pub struct DstContract {
     pub contract_address: String,
