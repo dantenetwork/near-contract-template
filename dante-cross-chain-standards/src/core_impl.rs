edd3d9c0eda384af33e08531779cc234b83ee162
Kay <kay20475@hotmail.com>
2022-05-12 16:31:35 +0800

    update dante-cross-chain-standards

--- dante-cross-chain-standards/src/core_impl.rs
+++ dante-cross-chain-standards/src/core_impl.rs
@@ -1,35 +1,30 @@
-use crate::core::CrossChainCore;
 use crate::types::{Content, DstContract, Session};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
-use near_sdk::{
-    env, ext_contract, AccountId, Balance, Gas, IntoStorageKey, Promise, PromiseOrValue,
-};
+use near_sdk::{ext_contract, AccountId, Balance, Gas, IntoStorageKey, PromiseOrValue};
 
 const GAS_FOR_SENT_MESSAGE: Gas = Gas(5_000_000_000_000);
 
 const NO_DEPOSIT: Balance = 0;
 
 #[ext_contract(ext_cross_contract)]
-pub trait FungibleTokenContract {
+pub trait CrossChainContract {
     fn send_message(&mut self, to_chain: String, content: Content, session: Option<Session>)
         -> u64;
 }
 
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Standards {
-    pub owner_id: AccountId,
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct CrossChain {
     pub cross_chain_contract_id: AccountId,
     pub destination_contract: UnorderedMap<String, DstContract>,
 }
 
-impl Standards {
-    pub fn new<S>(prefix: S, owner_id: AccountId, cross_chain_contract_id: AccountId) -> Self
+impl CrossChain {
+    pub fn new<S>(prefix: S, cross_chain_contract_id: AccountId) -> Self
     where
         S: IntoStorageKey,
     {
         let this = Self {
-            owner_id,
             cross_chain_contract_id,
             destination_contract: UnorderedMap::new(prefix),
         };
@@ -52,30 +47,16 @@ impl Standards {
         )
         .into()
     }
-}
 
-impl CrossChainCore for Standards {
-    fn register_dst_contract(
-        &mut self,
-        chain_name: String,
-        contract_address: String,
-        action_name: String,
-    ) {
-        assert_eq!(env::predecessor_account_id(), self.owner_id, "Unauthorize");
-        self.destination_contract.insert(
-            &chain_name,
-            &DstContract {
-                contract_address,
-                action_name,
-            },
-        );
-    }
-
-    fn call_cross(&self, to_chain: String, content: Content) {
+    pub fn call_cross(&self, to_chain: String, content: Content) {
         self.internal_call_cross_chain(to_chain, content, None);
     }
 
-    fn call_cross_with_session(&self, to_chain: String, content: Content) -> PromiseOrValue<u64> {
+    pub fn call_cross_with_session(
+        &self,
+        to_chain: String,
+        content: Content,
+    ) -> PromiseOrValue<u64> {
         self.internal_call_cross_chain(
             to_chain,
             content,
@@ -86,7 +67,7 @@ impl CrossChainCore for Standards {
         )
     }
 
-    fn send_response_message(&self, to_chain: String, content: Content, id: u64) {
+    pub fn send_response_message(&self, to_chain: String, content: Content, id: u64) {
         self.internal_call_cross_chain(
             to_chain,
             content,
@@ -94,6 +75,21 @@ impl CrossChainCore for Standards {
                 res_type: 2,
                 id: Some(id),
             }),
+        );
+    }
+
+    pub fn register_dst_contract(
+        &mut self,
+        chain_name: String,
+        contract_address: String,
+        action_name: String,
+    ) {
+        self.destination_contract.insert(
+            &chain_name,
+            &DstContract {
+                contract_address,
+                action_name,
+            },
         );
     }
 }
