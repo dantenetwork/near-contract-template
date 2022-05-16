77c8594e210f3cb297e0087e4a56a1b2eb14fa40
Kay <kay20475@hotmail.com>
2022-05-16 16:26:29 +0800

    update computing and greeting contracts

--- contracts/greeting/src/lib.rs
+++ contracts/greeting/src/lib.rs
@@ -1,4 +1,4 @@
-use dante_cross_chain_standards::{Content, CrossChain};
+use dante_cross_chain_standards::{Content, Context, CrossChain};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::serde::{Deserialize, Serialize};
@@ -17,14 +17,14 @@ pub struct GreetingData {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Greeting {
-    owner_id: AccountId,
     cross: CrossChain,
     greeting_data: UnorderedMap<String, GreetingData>,
 }
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
-    CrossChain,
+    DestinationContract,
+    PermittedContract,
     GreetingData,
 }
 
@@ -33,8 +33,12 @@ impl Greeting {
     #[init]
     pub fn new(owner_id: AccountId, cross_chain_contract_id: AccountId) -> Self {
         Self {
-            owner_id,
-            cross: CrossChain::new(StorageKey::CrossChain, cross_chain_contract_id),
+            cross: CrossChain::new(
+                owner_id,
+                StorageKey::DestinationContract,
+                StorageKey::PermittedContract,
+                cross_chain_contract_id,
+            ),
             greeting_data: UnorderedMap::new(StorageKey::GreetingData),
         }
     }
@@ -69,11 +73,16 @@ impl Greeting {
         self.cross.call_cross(to_chain, content);
     }
 
-    pub fn receive_greeting(&mut self, greeting: Vec<String>) {
+    pub fn receive_greeting(&mut self, greeting: Vec<String>, context: Context) {
         assert_eq!(
             env::predecessor_account_id(),
             self.cross.cross_chain_contract_id,
             "Processs by cross chain contract"
+        );
+        self.cross.assert_register_permitted_contract(
+            &context.from_chain,
+            &context.sender,
+            &context.action,
         );
         let data = GreetingData {
             from_chain: greeting[0].clone(),
@@ -87,15 +96,6 @@ impl Greeting {
     pub fn get_greeting(&self, from_chain: String) -> Option<GreetingData> {
         self.greeting_data.get(&from_chain)
     }
-
-    pub fn register_dst_contract(
-        &mut self,
-        chain_name: String,
-        contract_address: String,
-        action_name: String,
-    ) {
-        assert_eq!(env::predecessor_account_id(), self.owner_id, "Unauthorize");
-        self.cross
-            .register_dst_contract(chain_name, contract_address, action_name);
-    }
 }
+
+dante_cross_chain_standards::impl_cross_chain_register!(Greeting, cross);
